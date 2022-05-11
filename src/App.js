87dcd37480bc0e0cdbdd7f8962b3a2d06e856751
skapoor68 <skapoor68@gatech.edu>
2022-05-11 14:28:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+const api = {
+  key: "ac7e9009effe6d900738463e7f8eb303",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
